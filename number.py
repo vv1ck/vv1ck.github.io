403603e891fe7e13f22cf6b68e85c49b879e1642
Mr.Joker <75469718+vv1ck@users.noreply.github.com>
2025-12-22 20:57:03 -0800

    Delete number.py

--- number.py
+++
@@ -1,7 +0,0 @@
-# By @vv1ck @TweakPY
-import requests
-# abbreviation for tool
-print(
- requests.get('https://jftv.pythonanywhere.com/Number-Info/'+input('country code : ')+'/'+input('Enter number : ')
- ).text
-)
